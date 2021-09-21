91f95021d6346fa05d8f818e85fefa89e3398ad8
samuelalundborgcasumo <samuela.lundborg@casumo.com>
2021-09-21 15:08:17 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          frick yeah i might be a genius
         </p>
         <a
           className="App-link"
